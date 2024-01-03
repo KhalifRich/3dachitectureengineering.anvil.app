6c78edaa68839dec40cbbb8e60bd837562317ccd
calebkemboi99@gmail.com <calebkemboi99@gmail.com>
2024-01-03 08:50:24 +0000

    Edited Upload and settings

--- client_code/Upload/__init__.py
+++ client_code/Upload/__init__.py
@@ -1,5 +1,11 @@
 from ._anvil_designer import UploadTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
 class Upload(UploadTemplate):
   def __init__(self, **properties):
