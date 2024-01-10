b079b7e7b696ad2b6cdc9242bdeefca9a6ea85f2
calebkemboi99@gmail.com <calebkemboi99@gmail.com>
2024-01-10 14:20:49 +0000

    Edited Upload.Sign_up

--- client_code/Upload/Sign_up/__init__.py
+++ client_code/Upload/Sign_up/__init__.py
@@ -14,3 +14,30 @@ class Sign_up(Sign_upTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+      def first_name_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def second_name_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def phone_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def address_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def email_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def password_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def confirm_password_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+    
+
+    # Any code you write here will run before the form opens.
+def button_2_click(self, **event_args):
+    open_form('Registration')
+ pass
