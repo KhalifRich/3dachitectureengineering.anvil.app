1aa977a529dcadadc97314aa20ddc0deeecc982e
calebkemboi99@gmail.com <calebkemboi99@gmail.com>
2024-01-10 12:24:52 +0000

    Edited Registration

--- client_code/Registration/__init__.py
+++ client_code/Registration/__init__.py
@@ -16,16 +16,25 @@ class Registration(RegistrationTemplate):
      def first_name_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     pass
-     def email_pressed_enter(self, **event_args):
+     def second_name_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def phone_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def address_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     pass
      def email_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def password_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+     def confirm_password_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     pass
     
-    def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
 
     # Any code you write here will run before the form opens.
 def button_2_click(self, **event_args):
