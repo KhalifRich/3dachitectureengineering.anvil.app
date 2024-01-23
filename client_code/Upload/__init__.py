ec9a92533c4ae77fef9ba2c6793cfc7838979014
calebkemboi99@gmail.com <calebkemboi99@gmail.com>
2024-01-23 16:14:46 +0000

    Created ServerModule1; Edited Upload

--- client_code/Upload/__init__.py
+++ client_code/Upload/__init__.py
@@ -8,6 +8,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
+from anvil import js
 
 class Upload(UploadTemplate):
   def __init__(self, **properties):
@@ -21,4 +22,25 @@ class Upload(UploadTemplate):
     {
     'Upload':forms.FileInputs(attrs = {'accept':'.fbx, .stl, .iges, .obj, .collada, .step, .3ds'})
     }
+    pass
+
+  def subscribe_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    def subscribe_button_click(self, **event_args):
+        stripe_token = js.stripe.createToken(self.card_number, {})
+        amount = self.amount_box.text
+        result = anvil.server.call('process_payment', stripe_token['token']['id'], amount)
+        if 'client_secret' in result:
+            self.show_success_message()
+        else:
+            self.show_error_message(result)
+
+    def show_success_message(self):
+        # Implement success message handling
+        pass
+
+    def show_error_message(self, error):
+        # Implement error message handling
+        pass
+
     pass
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,38 @@
+import anvil.stripe
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+import stripe
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+stripe.api_key = 'your_stripe_secret_key'
+
+@anvil.server.callable
+def process_payment(token, amount):
+    try:
+        payment_intent = stripe.PaymentIntent.create(
+            amount=int(amount * 100),  # Convert amount to cents
+            currency='usd',
+            payment_method=token,
+            confirmation_method='manual',
+            confirm=True,
+        )
+        return payment_intent.client_secret
+    except stripe.error.CardError as e:
+        # Handle card errors
+        return str(e)
