8d0cee9f2e9672607a9a16da9b63fc259334f6f9
calebkemboi99@gmail.com <calebkemboi99@gmail.com>
2024-02-05 13:10:38 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,39 +1,42 @@
-import anvil.stripe
-import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from ._anvil_designer import Sign_InTemplate
+from anvil import *
+import stripe.checkout
+import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
-import stripe
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
-stripe.api_key = 'pk_test_51OVEBOJSA1HIvKzySaMh8c9BmuUOahNmC0zRj2m6QxqQ1wLBFvzUo74KYRxbqiutVg7rVMCAiYXBsFTaaHb7nwVo00305E7PZH'
+class Sign_In(Sign_InTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
 
-@anvil.server.callable
-def process_payment(token, amount):
-    try:
-        payment_intent = stripe.PaymentIntent.create(
-            amount=int(amount * 100),  # Convert amount to cents
-            currency='usd',
-            payment_method=token,
-            confirmation_method='manual',
-            confirm=True,
-        )
-        return payment_intent.client_secret
-      
-    except stripe.error.CardError as e:
-        # Handle card errors
-        return str(e)
+    # Any code you write here will run before the form opens.
+
+  def email_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def password_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def email_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def sign_in_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+    # Any code you write here will run before the form opens.
+
+  def register_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Register')
+    pass
+    
+
+  
