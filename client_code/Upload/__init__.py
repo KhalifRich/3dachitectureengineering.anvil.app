8a6f2c9560bb94d355cd7e169e0a5a32536082b9
calebkemboi99@gmail.com <calebkemboi99@gmail.com>
2024-02-05 16:16:29 +0000

    Edited Upload

--- client_code/Upload/__init__.py
+++ client_code/Upload/__init__.py
@@ -15,7 +15,7 @@ class Upload(UploadTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
-  def file_loader_1_change(self, file, **event_args):
+  def file_loader_1_attrs(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
     {
     'Upload':forms.FileInputs(attrs = {'accept':'.fbx, .stl, .iges, .obj, .collada, .step, .3ds'})
