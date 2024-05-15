57077e498dc2a4ea9f6811887acde0168400911c
calebkemboi99@gmail.com <calebkemboi99@gmail.com>
2024-05-15 11:26:23 +0000

    Edited UploadForm.SignInForm

--- client_code/UploadForm/SignInForm/__init__.py
+++ client_code/UploadForm/SignInForm/__init__.py
@@ -27,11 +27,36 @@ class SignInForm(SignInFormTemplate):
             alert("Incorrect email or password.")
 
     # Function to handle opening the sign-in form
-    def open_sign_in_form():
-        open_form('SignInForm')
+    def open_sign_in_form(self):
+        open_form('SignInForm')  # Corrected the function definition to include 'self'
 
     # Linking button click event to JavaScript function
     def button_sign_in_click(self, **event_args):
         js.open_sign_in_form()
+
+    # Method to handle enter key press in username text box
+    def text_box_1_pressed_enter(self, **event_args):
+        """This method is called when the user presses Enter in the username text box"""
+        # Fetch the username entered by the user
+        username = self.text_box_1.text
+        
+        # Move the focus to the email text box
+        self.text_box_2.focus()
+
+    # Method to handle enter key press in email text box
+    def text_box_2_pressed_enter(self, **event_args):
+        """This method is called when the user presses Enter in the email text box"""
+        # Fetch the email entered by the user
+        email = self.text_box_2.text
+
+        # Attempt to sign in the user
+        try:
+            user = app_tables.users.get(email=email)
+            
+            # If the user exists, do something (e.g., navigate to another form)
+            open_form('UploadForm')  # Replace 'HomeForm' with the name of your home form
+        except anvil.users.NoSuchUserError:
+            # If the user does not exist, display an error message
+            alert("User does not exist. Please check your email or sign up.")
 
     # Other code remains unchanged
