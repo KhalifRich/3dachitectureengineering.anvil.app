4b2c5f8258c1185af547da1a2f04464e65f9cdf4
calebkemboi99@gmail.com <calebkemboi99@gmail.com>
2024-06-10 11:16:40 +0000

    Edited ServerModule

--- server_code/ServerModule.py
+++ server_code/ServerModule.py
@@ -7,15 +7,47 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+@anvil.server.callable
+def create_user(first_name, second_name, phone, address, email, password):
+    existing_user = app_tables.users.get(email=email)
+    if existing_user:
+        raise anvil.users.UserExists("A user with this email already exists.")
+    app_tables.users.add_row(first_name=first_name, second_name=second_name, phone=phone,
+                             address=address, email=email, password=anvil.users.generate_password_hash(password))
+
+@anvil.server.callable
+def authenticate_user(email, password):
+    user = app_tables.users.get(email=email)
+    if user and anvil.users.check_password(password, user['password']):
+        return user
+    else:
+        return None
+
+@anvil.server.callable
+def initiate_download(product_id):
+    product_details = app_tables.products.get(id=product_id)
+    session = anvil.stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'usd',
+                'product_data': {
+                    'name': product_details['name'],
+                },
+                'unit_amount': product_details['price'] * 100,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=anvil.server.get_api_origin() + '/checkout/success',
+        cancel_url=anvil.server.get_api_origin() + '/checkout/cancel',
+    )
+    return session.id
+
+@anvil.server.http_endpoint('/checkout/success')
+def checkout_success():
+    return {'status': 'success'}
+
+@anvil.server.http_endpoint('/checkout/cancel')
+def checkout_cancel():
+    return {'status': 'cancelled'}
